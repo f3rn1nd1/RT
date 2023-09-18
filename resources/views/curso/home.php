0ca0e19e1587871dc1395a10c0777990fcdfd69b
f3rn1nd1 <mby301101@gmail.com>
2023-09-18 15:30:26 -0300

    Se agrega el proyecto RT

---
+++ resources/views/curso/home.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JOBQUISTER</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <img src="logo.png" alt="" width="60" height="60">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarTogglerDemo02">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="login.html">Crea cuenta</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="nosotros.html">Quienes Somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Ofertas laborales</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <section class="seccion ">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 pt-5 pe-6 mt-5">
+                    <h2 class="">JOBQUISTER</h2>
+                    <p class="pt-3 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi doloribus quos
+                        deleniti alias
+                        consequuntur voluptate explicabo neque, optio ipsam iusto inventore reprehenderit pariatur illum
+                        iure cum! Fuga dolorum quas repudiandae!</p>
+                    <p>Lorem ipsum dolor, sit amet conssectetur adipisicing elit. Placeat, assumenda. Consequuntur
+                        temporibus enim doloremque dolorem earum, tempore deleniti et a vero ex dolor illum porro
+                        blanditiis, eius obcaecati error doloribus.</p>
+                </div>
+                <div class="col-6 pt-5">
+                    <img src="UI-UX team-rafiki.svg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="mb-3">
+                        <label class="pb-3" for="select1">Elige una opcion</label>
+                        <select class="form-select" id="select1">
+                            <option value="1">Ingeniero</option>
+                            <option value="2">Medico</option>
+                            <option value="3">Enfermera</option>
+                            <option value="4">Abogado</option>
+                            <option value="5">Operador</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"
+            integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa"
+            crossorigin="anonymous"></script>
+</body>
+
+</html>
